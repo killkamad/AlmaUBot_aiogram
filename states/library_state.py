d016c57c4deeef3685b2f7348a554a413c66f499
denkleman <daniyar.urazbayev99@gmail.com>
2020-11-12 14:02:54 +0600

    fix email send

--- states/library_state.py
+++ states/library_state.py
@@ -2,4 +2,7 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
 class EmailReg(StatesGroup):
+    bookbase = State()
     names = State()
+    email = State()
+    phone = State()

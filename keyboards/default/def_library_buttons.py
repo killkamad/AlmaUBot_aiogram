0ddac168e4a7c31ae7853ca8b671f275587e3f32
Kirill <killka_m@mail.ru>
2020-11-15 18:09:35 +0600

    Поменял эмодзи в меню библиотеки

--- keyboards/default/def_library_buttons.py
+++ keyboards/default/def_library_buttons.py
@@ -3,15 +3,15 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def keyboard_library():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    callback_button1 = KeyboardButton(text="📕 Вебсайт")
-    callback_button2 = KeyboardButton(text="💡 Электронные ресурсы")
+    callback_button1 = KeyboardButton(text="🌐 Вебсайт")
+    callback_button2 = KeyboardButton(text="⚡ Электронные ресурсы")
     callback_button3 = KeyboardButton(text="☎ Контакты")
     callback_button4 = KeyboardButton(text="🕐 Время работы")
-    callback_button7 = KeyboardButton(text="💻 Онлайн курсы")
+    callback_button7 = KeyboardButton(text="🎓 Онлайн курсы")
     callback_button8 = KeyboardButton(text="💳 Потерял(a) ID-карту")
-    callback_button9 = KeyboardButton(text="📛 Правила")
+    callback_button9 = KeyboardButton(text="⚠ Правила")
     callback_button10 = KeyboardButton(text="📰 Права читателя")
-    callback_button11 = KeyboardButton(text="❌ Что не разрешается")
+    callback_button11 = KeyboardButton(text="🚫 Что не разрешается")
     callback_button12 = KeyboardButton(text="⛔ Ответственность за нарушения")
     callback_button13 = KeyboardButton(text="⬅ В главное меню")
     markup.add(callback_button1, callback_button2, callback_button3, callback_button4,

4ee0fa0f4796d56c6d3a4eebe99ba566e9925777
denkleman <daniyar.urazbayev99@gmail.com>
2021-02-13 12:12:19 +0600

    Добавил Профессорско-преподавательский состав

--- handlers/users/admin_navigation.py
+++ handlers/users/admin_navigation.py
@@ -3,12 +3,12 @@ import logging
 
 from aiogram.utils import exceptions
 from aiogram import types
-from aiogram.types import CallbackQuery, ContentType
+from aiogram.types import CallbackQuery, ContentType, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from loader import dp, bot
 from keyboards.inline.admin_buttons import inline_keyboard_nav_university_admin_menu, inline_keyboard_contact_center_admin, \
     cancel_or_send_contact_center_admin, cancel_or_update_contact_center_admin, \
     cancel_or_delete_contact_center_admin, inline_keyboard_contacts_center_delete, inline_keyboard_contacts_center_update, \
-    inline_keyboard_cancel_contact_center_admin
+    inline_keyboard_cancel_contact_center_admin, cancel_or_send_tutors_management 
 
 import asyncio
 
@@ -17,7 +17,7 @@ from .admin_menu import admin_menu
 
 from utils.delete_messages import bot_delete_messages
 from aiogram.dispatcher import FSMContext
-from states.admin import SendContactCenter, UpdateContactCenter, DeleteContactCenter
+from states.admin import SendContactCenter, UpdateContactCenter, DeleteContactCenter, Pps_admin
 
 
 @dp.callback_query_handler(text_contains='nav_university_admin_menu')
@@ -47,6 +47,7 @@ async def callback_inline_cancel_step(call: CallbackQuery, state: FSMContext):
     logging.info(f'User({call.message.chat.id}) нажал на кнопку {call.data}')
     await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                                 text='<b><i>ОТМЕНЕНО</i></b>', parse_mode='HTML')
+    await call.message.answer('Админ меню навигации', reply_markup = inline_keyboard_nav_university_admin_menu())
     await state.reset_state()
 
 
@@ -174,3 +175,90 @@ async def callback_inline_send_schedule(call: CallbackQuery, state: FSMContext):
     except Exception as e:
         await call.message.answer(f'(Ошибка - {e})')
         logging.info(f'Ошибка - {e}')
+@dp.callback_query_handler(text='tutors_university_admin', state=None)
+async def callback_tutors_university_admin_state(call: CallbackQuery):
+    await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                text='выбор для изменений', reply_markup=inline_keyboard_cancel_contact_center_admin())
+    # await bot.send_message(chat_id=message.chat.id, text='ВЫБИРЕТЕ Школу', reply_markup=inline_keyboard_cancel_contact_center_admin())
+    await call.message.answer('Выберите школу для которой хотите ввести изменения\n'
+                              'Для отмены нажмите сверху кнопку отмены',
+                              reply_markup=keyboard_pps_choice_shcool())
+    await Pps_admin.shcool.set()
+
+
+def keyboard_pps_choice_shcool():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add("Школа менеджмента", "Школа политики и права")
+    markup.add("Школа Инженерного Менеджмента", "Школа предпринимательства и инноваций")
+    markup.add("Высшая Школа Бизнеса", "Школа Экономики и Финансов")
+    markup.add("Ректорат")
+    return markup
+
+
+@dp.message_handler(
+    lambda message: message.text in ['Школа менеджмента', 'Школа политики и права', 'Школа Инженерного Менеджмента', 'Школа предпринимательства и инноваций', 'Высшая Школа Бизнеса', 'Школа Экономики и Финансов'],
+    state=Pps_admin.shcool)
+async def tutors_university_admin_state_shcool(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['shcool'] = message.text
+    await bot.send_message(chat_id=message.chat.id, text='Выбор для изменений',
+                           reply_markup=inline_keyboard_cancel_contact_center_admin())
+    await message.reply("выбирете что хотите изменить", reply_markup=keyboard_pps_choice_position())
+    await Pps_admin.position.set()
+
+
+@dp.message_handler(
+    lambda message: message.text in ['Ректорат'],
+    state=Pps_admin.shcool)
+async def tutors_university_admin_state_rectorat(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['shcool'] = message.text
+    await bot.send_message(chat_id=message.chat.id, text='Выбор для изменений',
+                           reply_markup=inline_keyboard_cancel_contact_center_admin())
+    await message.reply("выбирете что хотите изменить", reply_markup=keyboard_pps_choice_position_rector())
+    await Pps_admin.position.set()
+
+
+def keyboard_pps_choice_position():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add("Декан", "Преподаватели")
+    return markup
+
+
+def keyboard_pps_choice_position_rector():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add("Ректор", "Проректоры")
+    return markup
+
+@dp.message_handler(lambda message: message.text in ['Декан', 'Преподаватели', "Ректор", "Проректоры"],state=Pps_admin.position)
+async def tutors_university_admin_state_position(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['position'] = message.text
+    await message.reply(f"Напишите описание которое хотите изменить для {data['position']} {data['shcool']}", reply_markup=ReplyKeyboardRemove())
+    await Pps_admin.description.set()
+
+
+@dp.message_handler(content_types=ContentType.ANY, state=Pps_admin.description)
+async def message_send_tutors_management(message: types.Message, state: FSMContext):
+    if message.content_type == 'text':
+        await state.update_data(description = message.text.lower(), user_id=message.chat.id)
+        data = await state.get_data()
+        await bot.send_message(message.chat.id, text='Отправить это описание?', reply_markup=cancel_or_send_tutors_management())
+        await state.reset_state(with_data=False)
+    else:
+        await message.answer('Ошибка - вы отправили не текст повторите')
+
+
+@dp.callback_query_handler(text='send_tutors_management', state=None)
+async def callback_inline_send_tutors_management_final(call: CallbackQuery, state: FSMContext):
+    try:
+        data = await state.get_data()
+        await bot_delete_messages(call.message, 9)
+        await db.add_pps_data(data['user_id'], data['shcool'], data['position'], data['description'])
+        await bot.delete_message(call.message.chat.id, call.message.message_id)
+        await call.message.answer(f'описание отправлено')
+        await call.message.answer('Админ меню навигации', reply_markup = inline_keyboard_nav_university_admin_menu())
+        logging.info(f'User({call.message.chat.id}) отправил информацию для преподавателей менеджмента')
+    except Exception as e:
+        await call.message.answer(f'Ошибка описание не отправлено, (Ошибка - {e})')
+        print(e)
--- keyboards/inline/navigation_buttons.py
+++ keyboards/inline/navigation_buttons.py
@@ -24,12 +24,139 @@ async def inline_keyboard_contacts_center():
         call_list.append(callback_data)
     markup.add(*[InlineKeyboardButton(text=button, callback_data=call_data) for button, call_data in
                  zip(name, call_list)])
-    markup.add(InlineKeyboardButton(text="⬅ Назад", callback_data="go_back"))
+    markup.add(InlineKeyboardButton(text="⬅ Назад", callback_data="/nav_unifi"))
     return markup
 
 
 def inline_keyboard_contacts_center_back():
     markup = InlineKeyboardMarkup(row_width=1)
     callback_button = InlineKeyboardButton(text="Назад", callback_data='contacts_center')
+    markup.add(callback_button)
+    return markup
+
+
+# Кнопки Профессорско-преподавательский состав
+def inline_keyboard_pps():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="Школа менеджмента", callback_data='shcool_management')
+    callback_button1 = InlineKeyboardButton(text="Школа политики и права", callback_data='shcool_law')
+    callback_button2 = InlineKeyboardButton(text="Школа Инженерного Менеджмента",callback_data='shcool_engineer')
+    callback_button3 = InlineKeyboardButton(text="Школа предпринимательства и инноваций", callback_data='shcool_inovation')
+    callback_button4 = InlineKeyboardButton(text="Высшая Школа Бизнеса", callback_data='shcool_bussines')
+    callback_button5 = InlineKeyboardButton(text="Школа Экономики и Финансов", callback_data='shcool_economic')
+    callback_button6 = InlineKeyboardButton(text="Ректорат", callback_data='rectorat')
+    callback_button7 = InlineKeyboardButton(text="⬅ Назад", callback_data="/nav_unifi")
+    markup.add(callback_button, callback_button1, callback_button2, callback_button3, callback_button4, callback_button5, callback_button6, callback_button7)
+    return markup
+
+
+def inline_keyboard_pps_shcool_management():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="Декан", callback_data='dekan_shcool_management')
+    callback_button1 = InlineKeyboardButton(text="Преподаватели", callback_data='tutors_shcool_management')
+    callback_button2 = InlineKeyboardButton(text="⬅ Назад", callback_data='tutors_university')
+    markup.add(callback_button, callback_button1, callback_button2)
+    return markup
+
+
+def inline_keyboard_pps_shcool_law():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="Декан", callback_data='dekan_shcool_law')
+    callback_button1 = InlineKeyboardButton(text="Преподаватели", callback_data='tutors_shcool_law')
+    callback_button2 = InlineKeyboardButton(text="⬅ Назад", callback_data='tutors_university')
+    markup.add(callback_button, callback_button1, callback_button2)
+    return markup
+
+
+def inline_keyboard_pps_shcool_engineer():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="Декан", callback_data='dekan_shcool_engineer')
+    callback_button1 = InlineKeyboardButton(text="Преподаватели", callback_data='tutors_shcool_engineer')
+    callback_button2 = InlineKeyboardButton(text="⬅ Назад", callback_data='tutors_university')
+    markup.add(callback_button, callback_button1, callback_button2)
+    return markup
+
+
+def inline_keyboard_pps_shcool_inovation():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="Декан", callback_data='dekan_shcool_inovation')
+    callback_button1 = InlineKeyboardButton(text="Преподаватели", callback_data='tutors_shcool_inovation')
+    callback_button2 = InlineKeyboardButton(text="⬅ Назад", callback_data='tutors_university')
+    markup.add(callback_button, callback_button1, callback_button2)
+    return markup
+
+
+def inline_keyboard_pps_shcool_bussines():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="Декан", callback_data='dekan_shcool_bussines')
+    callback_button1 = InlineKeyboardButton(text="Преподаватели", callback_data='tutors_shcool_bussines')
+    callback_button2 = InlineKeyboardButton(text="⬅ Назад", callback_data='tutors_university')
+    markup.add(callback_button, callback_button1, callback_button2)
+    return markup
+
+
+def inline_keyboard_pps_shcool_economic():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="Декан", callback_data='dekan_shcool_economic')
+    callback_button1 = InlineKeyboardButton(text="Преподаватели", callback_data='tutors_shcool_economic')
+    callback_button2 = InlineKeyboardButton(text="⬅ Назад", callback_data='tutors_university')
+    markup.add(callback_button, callback_button1, callback_button2)
+    return markup
+
+
+def inline_keyboard_pps_rectorat():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="Ректор", callback_data='rectorat_rector')
+    callback_button1 = InlineKeyboardButton(text="Проректор", callback_data='rectorat_humans')
+    callback_button2 = InlineKeyboardButton(text="⬅ Назад", callback_data='tutors_university')
+    markup.add(callback_button, callback_button1, callback_button2)
+    return markup
+
+
+def inline_keyboard_pps_shcool_management_back():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="⬅ Назад", callback_data='shcool_management')
+    markup.add(callback_button)
+    return markup
+
+
+def inline_keyboard_pps_shcool_law_back():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="⬅ Назад", callback_data='shcool_law')
+    markup.add(callback_button)
+    return markup
+
+
+def inline_keyboard_pps_shcool_engineer_back():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="⬅ Назад", callback_data='shcool_engineer')
+    markup.add(callback_button)
+    return markup
+
+
+def inline_keyboard_pps_shcool_inovation_back():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="⬅ Назад", callback_data='shcool_inovation')
+    markup.add(callback_button)
+    return markup
+
+
+def inline_keyboard_pps_shcool_bussines_back():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="⬅ Назад", callback_data='shcool_bussines')
+    markup.add(callback_button)
+    return markup
+
+
+def inline_keyboard_pps_shcool_economic_back():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="⬅ Назад", callback_data='shcool_economic')
+    markup.add(callback_button)
+    return markup
+
+
+def inline_keyboard_pps_rectorat_back():
+    markup = InlineKeyboardMarkup(row_width=1)
+    callback_button = InlineKeyboardButton(text="⬅ Назад", callback_data='rectorat')
     markup.add(callback_button)
     return markup
